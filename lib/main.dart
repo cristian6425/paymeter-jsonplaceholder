9d5f6830d77927ebadcb4e9b24771bd40c400db5
cristian6425 <cristianchacondt@gmail.com>
2025-11-09 17:05:50 -0500

    add use case base classes and improve main function formatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:paymeterjsonplaceholder/config/environment.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:paymeterjsonplaceholder/config/environment.dart';
+
 import 'app/app.dart';
 
 void main() {
   const String environment = String.fromEnvironment('app.flavor');
   Environment().initConfig(EnvironmentType.fromString(environment));
   WidgetsFlutterBinding.ensureInitialized();
-  initializeDateFormatting('es', null).then((_) => runApp(const ProviderScope(child: MyApp())));
+  initializeDateFormatting('es', null).then(
+    (_) => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    ),
+  );
 }
